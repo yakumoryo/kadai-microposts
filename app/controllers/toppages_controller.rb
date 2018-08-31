fc086ec04699a20242ff34d61f7bfe3883c49050
EC2 Default User <ec2-user@ip-172-31-25-202.us-east-2.compute.internal>
2018-08-31 02:56:46 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,7 @@
 class ToppagesController < ApplicationController
   def index
+    @user = current_user
+      @micropost = current_user.microposts.build  # form_for 用
+      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
   end
 end
